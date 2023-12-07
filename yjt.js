5d6bf311881b7434ec87c687ff27c77c45afc40a
Andyeason <631550588@qq.com>
2023-12-07 22:15:44 +0800

    易截图2

---
+++ yjt.js
@@ -0,0 +1,12 @@
+[rewrite_local]
+^https?:\/\/(jietu6|1jietu)\.com\/apiv2\/user url script-response-body https://raw.githubusercontent.com/andyeasons/quanx/main/yjt.js
+
+[MITM]
+hostname = jietu6.com, 1jietu.com
+
+var iosjs = JSON.parse($response.body);
+iosjs.info.group_id = "3";
+iosjs.info.vip = "1";
+iosjs.info.group = "你死了之后就到期";
+iosjs.info.nickname = "Andyeason";
+$done({ body: JSON.stringify(iosjs) });
