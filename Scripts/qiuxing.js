f39560f1c99cd76479d32eab0eeb8eeee5aa44ae
Andyeason <631550588@qq.com>
2023-12-16 23:14:16 +0800

    Update qiuxing.js

--- Scripts/qiuxing.js
+++ Scripts/qiuxing.js
@@ -1,11 +1,8 @@
 //2023/12/16
 // ^https?:\/\/qiuxinkaifa\.com\/index\.php\/Api\/activity\/StarDetail
 // 解锁球星数据
-const url = $request.url;
 var body = $response.body; 
 console.log(`解锁球星数据前----: ` + body);
-
 body = body.replace("\"pubver\":0","\"pubver\":3");
 console.log(`解锁球星数据后----: ` + body);
-
 $done(body); 
