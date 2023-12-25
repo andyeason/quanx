324f928f1d721a595964fb971ea1889da6475b97
andyeason <631550588@qq.com>
2023-12-25 13:56:38 +0800

    update

--- Scripts/51card.js
+++ Scripts/51card.js
@@ -17,5 +17,19 @@ if (url.includes("api.u51.com/generic-config-gateway/api")) {
   }
 }
 
+if (url.includes("api.u51.com/externalgateway/api/v1/configuration-items/parent/gjBottomCfg")) {
+  obj?.configParentList[0]?.parentConfigItemList[0]?.itemMap.forEach(item => {
+  delete item.shop; 
+});
+  obj?.configParentList[0]?.parentConfigItemList[1]?.itemMap.forEach(item => {
+  delete item.shop; 
+  delete item.shop_AN1; 
+  delete item.shop_AN2; 
+  delete item.shopSelected; 
+});
+}
+
+
+
 body = JSON.stringify(obj);
 $done({body});
