1288ce5a0d902ce81f36a58152916caa00db9853
andyeason <631550588@qq.com>
2024-03-25 13:05:16 +0800

    update

--- Scripts/shotool.js
+++ Scripts/shotool.js
@@ -1,37 +1,14 @@
-// 
-
 if (!$response.body) {
   $done({});
 }
 const url = $request.url;
 let obj = JSON.parse($response.body);
-console.log(obj);
 
-if (url.includes("mod=banners")) {
-  if (obj?.data) {
-	obj.data = [];
-  }
-}
 if (url.includes("mod=config")) {
   if (obj?.data) {
 	delete obj.data.modalNotice;
-
   }
 }
-
-//if (url.includes("api.u51.com/externalgateway/api/v1/configuration-items/parent/gjBottomCfg")) {
-//  obj?.configParentList[0]?.parentConfigItemList[0]?.itemMap.forEach(item => {
-//  delete item.shop; 
-//});
-//  obj?.configParentList[0]?.parentConfigItemList[1]?.itemMap.forEach(item => {
-//  delete item.shop; 
-//  delete item.shop_AN1; 
-//  delete item.shop_AN2; 
-//  delete item.shopSelected; 
-//});
-//}
-
-
 
 body = JSON.stringify(obj);
 $done({body});
