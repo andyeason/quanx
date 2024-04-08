6888d88dfb436d7684a0c4b342451f0926bb9734
Andyeason <631550588@qq.com>
2024-04-09 00:22:15 +0800

    Update shotool.js

--- Scripts/shotool.js
+++ Scripts/shotool.js
@@ -1,16 +1,6 @@
+let body = $response.body;
+let obj = JSON.parse(body);
 
-// 2024年4月9日 00:08:11
-/**
-var Q = JSON.parse($response.body);
-Q.data.vipExpire = "2099-12-31 23:59:59";
-Q.data.nickName = "破解成功";
-console.log(JSON.stringify(Q));
-$done({body : JSON.stringify(Q)});**/
+obj.data.vipExpire = "2099-12-31 10:30:00";
 
-const url = $request.url;
-if (!$response.body) $done({});
-let obj = JSON.parse($response.body);
-obj.data.vipExpire = "2099-12-31 23:59:59";
-obj.data.nickName = "破解成功";
-console.log(`课程表解锁----: `+JSON.stringify(obj) );
-$done({ body: JSON.stringify(obj) });
+$done({body: JSON.stringify(obj)});
