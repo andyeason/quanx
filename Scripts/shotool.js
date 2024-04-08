34b268e55079c12c939a662d5b6720aad0d841c5
Andyeason <631550588@qq.com>
2024-04-09 00:26:40 +0800

    Update shotool.js

--- Scripts/shotool.js
+++ Scripts/shotool.js
@@ -1,6 +1,10 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
+console.log("原始数据：", obj);
+
 obj.data.vipExpire = "2099-12-31 10:30:00";
+
+console.log("修改后的数据：", obj);
 
 $done({body: JSON.stringify(obj)});
