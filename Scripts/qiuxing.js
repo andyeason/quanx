21fbfd9da20a06bea50d2cb3b689eea683b0146c
Andyeason <631550588@qq.com>
2024-04-21 12:30:36 +0800

    update 球星

--- Scripts/qiuxing.js
+++ Scripts/qiuxing.js
@@ -3,6 +3,11 @@
 // 解锁球星数据
 var body = $response.body; 
 //console.log(`解锁球星数据前----: ` + body);
-body = body.replace("\"pubver\":0","\"pubver\":3").replace("\"pubverTeam\":0","\"pubverTeam\":3");
+var url = $request.url;
+if (url.includes("Activity")) {
+	body = body.replace("\"pubver\":0","\"pubver\":2").replace("\"pubverTeam\":0","\"pubverTeam\":3");
+}else{
+	body = body.replace("\"pubver\":0","\"pubver\":3").replace("\"public1\":0","\"public1\":3").replace("\"oldver\":0","\"oldver\":3");
+}
 //console.log(`解锁球星数据后----: ` + body);
 $done(body); 
