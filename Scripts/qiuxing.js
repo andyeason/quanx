373d002564e5ca2f8400682f7d79da21e02f1fb6
Andyeason <631550588@qq.com>
2024-04-21 12:48:39 +0800

    Update qiuxing.js

--- Scripts/qiuxing.js
+++ Scripts/qiuxing.js
@@ -1,5 +1,5 @@
 //2023年12月16日 23:18:43
 // 解锁球星数据
 var body = $response.body; 
-body = body.replace("\"pubver\":0","\"pubver\":3").replace("\"public1\":0","\"public1\":3").replace("\"oldver\":0","\"oldver\":3").replace("\"pubverTeam\":0","\"pubverTeam\":3");
+body = body.replace("\"pubver\":0","\"pubver\":2").replace("\"public1\":0","\"public1\":3").replace("\"oldver\":0","\"oldver\":3").replace("\"pubverTeam\":0","\"pubverTeam\":3");
 $done(body); 
